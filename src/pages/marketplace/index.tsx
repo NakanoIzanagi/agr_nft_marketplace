6a1a24cb1de9853b26ad6d4137aff42c8a41d9c3
BlackHole1225 <blackhole1225.dev@gmail.com>
2024-09-18 08:43:19 +0100

    fetch listed nfts 2

--- src/pages/marketplace/index.tsx
+++ src/pages/marketplace/index.tsx
@@ -1,32 +1,42 @@
 import type { NextPage } from "next";
 import { NftView } from "../../views";
 import HeadContent from "components/HeadContent";
-const { Connection, PublicKey,clusterApiUrl } = require('@solana/web3.js');
-import { Program } from '@project-serum/anchor';
-import idl from '../../../public/idl.json';
-import { AnchorProvider, web3 } from '@project-serum/anchor';
-import { useWallet, useConnection } from '@solana/wallet-adapter-react';
 
 
-
+// type NftListing = {
+//     token_id: string;
+//     price: number;
+//     creator: string;
+//     royalty_percentage: number;
+//     seller: string;
+//     is_sold: boolean;
+//   };
 
 const Whitepaper: NextPage = (props) => {
-    async function fetchListedNfts() {
-        const wallet = useWallet();
+    // async function fetchListedNfts() {
+    //     const wallet = useWallet();
     
-        let connection = new Connection(clusterApiUrl("devnet"));
-        const provider = new AnchorProvider(connection, wallet, {});
+    //     let connection = new Connection(clusterApiUrl("devnet"));
+    //     const provider = new AnchorProvider(connection, wallet, {});
     
     
-        // Replace with your program ID
-        const programId = new PublicKey('9yAxQ1szpE3ajaYSEtageTaF1bugC5JnT3sa1RNuFZ5y');
-        const program = new Program(idl, programId, provider);
+    //     // Replace with your program ID
+    //     const programId = new PublicKey('9yAxQ1szpE3ajaYSEtageTaF1bugC5JnT3sa1RNuFZ5y');
+    //     const program = new Program(idl, programId, provider);
     
-        // // Fetch all NFT listings
-        const listings = await program.account.nftListing.all();
-        console.log(listings);
-    }
-    fetchListedNfts();
+    //     // // Fetch all NFT listings
+    //     const listings = await program.account.nftListing.all();
+    //     const formattedListings: NftListing[] = listings.map((listing: any) => ({
+    //         token_id: listing.account.tokenId.toBase58(),
+    //         price: listing.account.price.toNumber(),
+    //         creator: listing.account.creator.toBase58(),
+    //         royalty_percentage: listing.account.royaltyPercentage,
+    //         seller: listing.account.seller.toBase58(),
+    //         is_sold: listing.account.isSold,
+    //       }));
+    //     console.log(formattedListings);
+    // }
+    // fetchListedNfts();
     return (
         <div className="w-full h-screen bg-black">
             <HeadContent />
